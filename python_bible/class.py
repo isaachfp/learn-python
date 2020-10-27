23b992e45a630c656d8bf6023b14e19ae933a9b0
Isaac Henriques <isaachfp@gmail.com>
2020-10-27 19:17:53 -0300

    Evolving python class studies

--- python_bible/class.py
+++ python_bible/class.py
@@ -3,6 +3,7 @@ class MyClass:
     "This is my second class"
     a = 10
     def func(self):
+        # o self indica que o objeto passa a si próprio como argumento
         print("Hello!")
 
 #output 10
@@ -13,3 +14,8 @@ print(MyClass.func)
 
 # doc string
 print(MyClass.__doc__)
+
+ob = MyClass()
+
+ob.func() # é o mesmo que escrever 'MyClass.func(ob)'
+print(ob.__doc__)
